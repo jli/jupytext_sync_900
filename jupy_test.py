c96090afdb462ea343badc4f573586a2877257b6
john li <jli@circularly.org>
2022-01-04 14:25:05 -0500

    add new notebook and companion script (created via jupyterlab). 2nd `git commit` attempt, since pre-commit jupytext removed the jupytext metadata from notebook

---
+++ jupy_test.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+'new notebook created via jupyterlab. with jupytext.toml, the companion .py is auto-created.'
